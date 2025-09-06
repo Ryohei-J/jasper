904d52d422255b4b04f3dfa8bfd1393d00346a5e
Ryohei Jin <ad.tgib.tsr@gmail.com>
2025-09-06 12:42:24 +0900

    証明書更新のシェルスクリプト変更

--- updateCert.sh
+++ updateCert.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# certbotで証明書を更新
+certbot certonly --authenticator dns-oci -d bscrape.com --agree-tos --force-renewal
+
+# 更新した証明書をOCIにインポート
 certificate_id="ocid1.certificate.oc1.ap-tokyo-1.amaaaaaavstm55ya55heqjmiyk7ngkixnmscxmewjrs3wichhf2xcwocqhpq"
 certificate_pem=$(cat /etc/letsencrypt/live/bscrape.com/cert.pem)
 cert_chain_pem=$(cat /etc/letsencrypt/live/bscrape.com/chain.pem)
