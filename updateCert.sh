dab2cfc15e77614bca905f523967ae9b6000e006
Ryohei Jin <ad.tgib.tsr@gmail.com>
2025-09-06 12:23:11 +0900

    証明書更新のシェルスクリプト作成

---
+++ updateCert.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+certificate_id="ocid1.certificate.oc1.ap-tokyo-1.amaaaaaavstm55ya55heqjmiyk7ngkixnmscxmewjrs3wichhf2xcwocqhpq"
+certificate_pem="/etc/letsencrypt/live/bscrape.com/cert.pem"
+cert_chain_pem="/etc/letsencrypt/live/bscrape.com/chain.pem"
+private_key_pem="/etc/letsencrypt/live/bscrape.com/privkey.pem"
+
+oci certs-mgmt certificate update-certificate-by-importing-config-details \
+    --certificate-id "$certificate_id"\
+    --certificate-pem "$certificate_pem"\
+    --cert-chain-pem "$cert_chain_pem"\
+    --private-key-pem "$private_key_pem"
+    
