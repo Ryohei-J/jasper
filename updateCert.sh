cf2d3960e1bfb08875b070e41f613a9d17edee47
Ryohei Jin <ad.tgib.tsr@gmail.com>
2025-09-06 12:36:18 +0900

    証明書更新のシェルスクリプト修正

--- updateCert.sh
+++ updateCert.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 certificate_id="ocid1.certificate.oc1.ap-tokyo-1.amaaaaaavstm55ya55heqjmiyk7ngkixnmscxmewjrs3wichhf2xcwocqhpq"
-certificate_pem="/etc/letsencrypt/live/bscrape.com/cert.pem"
-cert_chain_pem="/etc/letsencrypt/live/bscrape.com/chain.pem"
-private_key_pem="/etc/letsencrypt/live/bscrape.com/privkey.pem"
+certificate_pem=$(cat /etc/letsencrypt/live/bscrape.com/cert.pem)
+cert_chain_pem=$(cat /etc/letsencrypt/live/bscrape.com/chain.pem)
+private_key_pem=$(cat /etc/letsencrypt/live/bscrape.com/privkey.pem)
 
 oci certs-mgmt certificate update-certificate-by-importing-config-details \
     --certificate-id "$certificate_id"\
     --certificate-pem "$certificate_pem"\
     --cert-chain-pem "$cert_chain_pem"\
     --private-key-pem "$private_key_pem"
-    
